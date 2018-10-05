01ed451c4c11df943afe4791ba6b0db5e42467e8
user <y.j.hanna@gmail.com>
2018-10-05 09:51:06 +0300

    games and players

---
+++ app/src/main/java/com/example/user/myapplication/Home.java
@@ -0,0 +1,33 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity implements View.OnClickListener{
+
+    //1.
+    Button btGames, btRating;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //2.
+        btGames = findViewById(R.id.btGames);
+        btGames.setOnClickListener(this);
+
+        btRating = findViewById(R.id.btRating);
+        btRating.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(btGames == v){
+
+        }else{
+
+        }
+    }
+}
--- app/src/main/java/com/example/user/myapplication/ListActivity.java
+++ app/src/main/java/com/example/user/myapplication/ListActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,10 +23,16 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
 
         lvcountries = (ListView) findViewById(R.id.lvcountries);
 
-        arrayList.add("Germany");
-        arrayList.add("France");
-        arrayList.add("Italy");
-
+        arrayList.add("Nadal");
+        arrayList.add("Federer");
+        arrayList.add("Djokovic");
+        arrayList.add("Juan Martin");
+        arrayList.add("Zverev");
+        arrayList.add("Marin");
+        arrayList.add("Dominic");
+        arrayList.add("Grigor");
+        arrayList.add("Anderson");
+        arrayList.add("Isner");
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
         lvcountries.setAdapter(arrayAdapter);
     }
@@ -33,11 +40,65 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         String selectedItem = arrayList.get(position);
-        if(selectedItem.equals("Germany")) {
-        //intent
-            //toast
+        Intent intent = new Intent(this, DetailsActivity.class);
+        if(selectedItem.equals("Nadal")) {
+            intent.putExtra("player","Nadal");
+        }else if(selectedItem.equals("Federer")){
+
         }
 
+        if(selectedItem.equals("Federer")) {
+            intent.putExtra("player","Federer");
+        }else if(selectedItem.equals("Djokovic")){
+
+        }
+
+        if(selectedItem.equals("Djokovic")) {
+            intent.putExtra("player","Djokovic");
+        }else if(selectedItem.equals("Juan Martin")) {
+
+        }
+
+        if(selectedItem.equals("Juan Martin")) {
+            intent.putExtra("player","Juan Martin");
+        }else if(selectedItem.equals("Zverev")) {
+
+        }
+
+        if(selectedItem.equals("Zverev")) {
+            intent.putExtra("player","Zverev");
+        }else if(selectedItem.equals("Marin")) {
+
+        }
+
+        if(selectedItem.equals("Marin")) {
+            intent.putExtra("player","Marin");
+        }else if(selectedItem.equals("Dominic")) {
+
+        }
+
+        if(selectedItem.equals("Dominic")) {
+            intent.putExtra("player","Dominic");
+        }else if(selectedItem.equals("Grigor")) {
+
+        }
+
+        if(selectedItem.equals("Grigor")) {
+            intent.putExtra("player","Grigor");
+        }else if(selectedItem.equals("Anderson")) {
+
+        }
+
+        if(selectedItem.equals("Anderson")) {
+            intent.putExtra("player","Anderson");
+        }else if(selectedItem.equals("Isner")) {
+
+        }
+
+        if(selectedItem.equals("Isner")) {
+            intent.putExtra("player","Isner");
+
+        startActivity(intent);
 
     }
 }
