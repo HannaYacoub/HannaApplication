9dd025e994063e326d351990431cab7db393cfa4
user <y.j.hanna@gmail.com>
2018-11-09 09:40:02 +0200

    list

--- app/src/main/java/com/example/user/myapplication/DetailsActivity.java
+++ app/src/main/java/com/example/user/myapplication/DetailsActivity.java
@@ -1,8 +1,20 @@
 package com.example.user.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -24,9 +36,45 @@ public class DetailsActivity extends AppCompatActivity {
         tvWeight = findViewById(R.id.tvWeight);
         tvLength = findViewById(R.id.tvLength);
         tvGrandslams = findViewById(R.id.tvGrandslams);
-        tvPartner = findViewById(R.id.tvPartner);
         tvFrenchopen = findViewById(R.id.tvFrenchopen);
 
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference refrence = database.getReference("Players");
+
+ /*       lvUsers = findViewById(R.id.lvUsers);
+        users = new ArrayList<>();
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, users);
+        lvUsers.setAdapter(adapter);*/
+
+        refrence.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Map<String, String> map = (Map<String, String>) dataSnapshot.getValue();
+                tvName.setText(map.get("Name"));
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/user/myapplication/FireActivity.java
+++ app/src/main/java/com/example/user/myapplication/FireActivity.java
@@ -1,11 +1,20 @@
 package com.example.user.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
+import java.util.Map;
 
 public class FireActivity extends AppCompatActivity {
 
@@ -18,12 +27,41 @@ public class FireActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire);
 
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference refrence = database.getReference("Players");
+
         lvUsers = findViewById(R.id.lvUsers);
         users = new ArrayList<>();
 
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, users);
+        lvUsers.setAdapter(adapter);
 
+        refrence.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Map<String, String> map = (Map<String, String>) dataSnapshot.getValue();
 
+            }
 
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
