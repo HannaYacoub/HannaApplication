01ee7d49bd36d2be94466f716b885e4f6be59369
user <y.j.hanna@gmail.com>
2018-11-21 12:06:08 +0200

    list

--- app/src/main/java/com/example/user/myapplication/ListActivity.java
+++ app/src/main/java/com/example/user/myapplication/ListActivity.java
@@ -25,16 +25,17 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
         lvcountries = (ListView) findViewById(R.id.lvTopTen);
 
 
-        players.add(new Player("Nadal", "22", "80", "1.80", "15",R.drawable.nadal));
-        players.add(new Player("Djokovic", "31", "80", "1.88", "14",R.drawable.djokovic));
-        players.add(new Player("Federer", "37", "80", "1.85", "20",R.drawable.federer));
-        players.add(new Player("Juan Martin", "30", "80", "1.98", "1",R.drawable.juanmartin));
-        players.add(new Player("Zverev", "21", "80", "1.98", "0",R.drawable.zverev));
-        players.add(new Player("Marin", "30", "80", "1.98", "1",R.drawable.marincilic));
-        players.add(new Player("Dominic", "25", "80", "1.85", "0",R.drawable.dominicthiem));
-        players.add(new Player("Anderson", "32", "80", "2.03", "0",R.drawable.anderson));
-        players.add(new Player("Isner", "33", "80", "2.8", "0",R.drawable.isner));
-        players.add(new Player("Grigor", "27", "80", "1.90", "0",R.drawable.grigor));
+        players.add(new Player("Rafael Nadal", "22", "80", "1.80", "15",R.drawable.nadal));
+        players.add(new Player("Novak Djokovic", "31", "80", "1.88", "14",R.drawable.djokovic));
+        players.add(new Player("Roger Federer", "37", "80", "1.85", "20",R.drawable.federer));
+        players.add(new Player("Juan Martin del Porto", "30", "80", "1.98", "1",R.drawable.juanmartin));
+        players.add(new Player("Alexander Zverev", "21", "80", "1.98", "0",R.drawable.zverev));
+        players.add(new Player("Marin Cilic", "30", "80", "1.98", "1",R.drawable.marincilic));
+        players.add(new Player("Dominic Thiem", "25", "80", "1.85", "0",R.drawable.dominicthiem));
+        players.add(new Player("Kevin Anderson", "32", "80", "2.03", "0",R.drawable.anderson));
+        players.add(new Player("john Isner", "33", "80", "2.8", "0",R.drawable.isner));
+        players.add(new Player("Grigor Dimitrov", "27", "80", "1.90", "0",R.drawable.grigor));
+
         arrayList.add("1. Nadal                                                                   7660");
         arrayList.add("2. Djokovic                                                              7445");
         arrayList.add("3. Federer                                                                6260");
